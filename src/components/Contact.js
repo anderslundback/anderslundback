bb919226c51e5e068c3f2928ba7d2f08f31b496f
Anders <lundback.anders@gmail.com>
2019-11-02 15:00:55 +0000

    contact page basic

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import '../css/Contact.css';
+
 
 const Contact = () => (
     <div>
         <h1>Contact</h1>
+        <p>You can reach me at <a href={"mailto:lundback.anders@gmail.com"}><strong>lundback.anders@gmail.com</strong></a></p>
     </div>
 )
 
